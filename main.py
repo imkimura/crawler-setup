e03b974e0c4a1585ab11552b8dcd86211849d107
imkimura <juliakimura@univem.edu.br>
2021-04-27 17:30:54 -0300

    modificacoes

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ from bs4 import BeautifulSoup as bsp
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 headers = {
-    'Accept-Encoding': 'gzip, deflate, sdch',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
@@ -29,57 +26,52 @@ if 'links.txt' in os.listdir(ROOT_DIR):
       try:
         
         session = requests.Session()
-        session.headers = CaseInsensitiveDict(headers)
-        # session.max_redirects = 130
-
+        session.headers['User-Agent'] = headers['User-Agent']
+        session.trust_env = False
         
-        if count != 1:
-          response = session.get(link, allow_redirects=False)
-          response = response.content
-          
-        else:
-          response = urlopen(Request(link, None, headers=headers))
+        responses = session.get(link.strip(), allow_redirects=False)
+        responses.raise_for_status()
+        response = responses.text
         
         soup = bsp(response, "html.parser")
         
+        print(responses.status_code)
+        
         if count == 0:
-          print(soup)
-          price = soup.find(class_='preco_desconto')
-
-          print(price.strong.text)
+          price = soup.select_one('.preco_desconto strong')
           
-        elif count == 1:
+          print(price.text)
           
-          price = soup.find(id='valVista')
+        elif count == 1:          
+          
+          price = soup.select_one('#valVista')
 
           print(price.text)
           
-          parse = soup.find(class_='val-parc')
-          
-          parse = parse.find(id='valParc')
+          parse = soup.select_one('.val-parc #valParc')          
 
           print(parse.text)
           
         elif count == 2:
           
-          price = soup.find(id='priceblock_ourprice')
+          price = soup.select_one('#priceblock_ourprice')
 
           print(price.text)
           
         elif count == 3:
           
-          price = soup.find(class_='price-boleto')
+          price = soup.select_one('.price-boleto span')
 
-          print(price.span.text)
+          print(price.text)
           
-          parse = soup.find(id='product-price-19108')
+          parse = soup.select_one('#product-price-19108 span')
 
-          print(parse.span.text)
+          print(parse.text)
           
       
       except Exception as e:
         
         print(e)
       
-      time.sleep(10)
+      time.sleep(5)
         
